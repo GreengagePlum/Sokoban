02dd4baae253bbb85e45cc9f77c8385263e60564
Efe ERKEN <efe.erken@etu.unistra.fr>
2022-11-08 01:08:20 +0100

    :wrench: UPDATE: Format and organize main.c
    Fixed various main.c formatting and indentation.

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include "grid.h"
 #include <stdbool.h>
-int main(void){
-	
-	bool run = true;
-	while(run){
-		char entry = fgetc(stdin);
-		switch(entry){
-			case 'q' :{
-				run = false;
-				break;
-			}
-		}
-	}
-}
+#include "grid.h"
 
+int main(void)
+{
+    bool run = true;
+    while (run)
+    {
+        char entry = fgetc(stdin);
+        switch (entry)
+        {
+        case 'q':
+        {
+            run = false;
+            break;
+        }
+        }
+    }
+}
