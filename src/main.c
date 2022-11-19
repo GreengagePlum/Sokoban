a7a9947fe3d5d202b0389c17f0af1f0ff05a5c2e
Efe ERKEN <efe.erken@etu.unistra.fr>
2022-11-19 13:37:05 +0100

    :sparkles: NEW: Add core game functionality
    The game is at a working state with a CLI

--- src/main.c
+++ src/main.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "grid.h"
+#include "player.h"
 
-int main(void)
+int main()
 {
+    grid *level = init_level("levels/level1.txt");
+    char entry = '\0';
     bool run = true;
     while (run)
     {
-        char entry = fgetc(stdin);
+        printf("\nAppuyez sur \"q\" pour quitter\n");
+        printf("Appuyez sur \"h, j, k, l\" pour vous déplacer\n\n");
+        display(level);
+        printf("Votre choix : ");
+        scanf(" %c", &entry);
         switch (entry)
         {
         case 'q':
-        {
             run = false;
             break;
-        }
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+            move_player(level, entry);
+            break;
+        default:
+            printf("---> Cette touche n'a pas de fonctionnalité\n");
         }
     }
+    free_level(level);
+    return EXIT_SUCCESS;
 }
