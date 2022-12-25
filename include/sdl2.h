5b2261fd640604602bf4d672da08f7d193e68dde
Efe ERKEN <efeerkenn@hotmail.com>
2022-12-25 18:18:54 +0000

    :wrench: UPDATE: Format sdl2.c and sdl2.h
    Formatted files to match project syntax and added documentation.

--- include/sdl2.h
+++ include/sdl2.h
@@ -1,30 +1,35 @@
-#pragma once
+#ifndef SDL2_H
+#define SDL2_H
+
+/**
+ * @file sdl2.h
+ * @brief Fichier header contenant la structure pour gérer @c SDL2 pour le jeu
+ * @version 0.2
+ * @date 2022-12-25
+ *
+ */
+
 #include <SDL2/SDL.h>
 
+/**
+ * @brief Structure contenant les informations pour l'affichage @c SDL2
+ *
+ * Cette structure contient les informations telles que la fenêtre, le renderer,
+ * la taille de la fenêtre @c SDL2 pour faciliter l'utilisation de cette bibliothèque
+ * dans le jeu.
+ */
 typedef struct SDLContext
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    int width;
-    int height;
+    SDL_Window *window;     ///< Pointeur sur la fenêtre
+    SDL_Renderer *renderer; ///< Pointeur sur le renderer
+    int width;              ///< Largeur de la fenêtre
+    int height;             ///< Hauteur de la fenêtre
 } SDLContext;
 
 extern SDLContext context;
 
-/* @brief
- * Initialise une variable global `context` de type SDLContext
- *
- * Si il y a erreur pendant l'intialisation:
- *  context.window = NULL
- *  ou
- *  context.renderer = NULL
- *
- * Les deux cas sont à testé !
- *
- */
 void sdl_init();
 
-/**
- * nettoie la variable global context
- */
 void sdl_quit();
+
+#endif
